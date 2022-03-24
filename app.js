00c7219bc73dee11a3f533c73e459f17c3f8d3d3
nexus122 <juan-pablo122@hotmail.es>
2022-03-24 09:50:42 +0100

    Ejercicio Solucionado

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // Happy coding!
+function bgColor(color){
+    document.body.style.backgroundColor = color;
+}
 
+document.querySelector('.canvas').addEventListener('click', function(event){
+    // Con el getComputedStyle podemos saber los colores aplicados a un elemento.
+    bgColor(getComputedStyle(event.target).backgroundColor);
+});
